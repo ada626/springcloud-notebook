22a815e6437559705376efe2e5611ba2d4b63ca2
ada626 <ada.yxin@gmail.com>
2022-01-16 12:31:29 +0800

    first model with basic config

---
+++ atguiguCloud/cloud-provider-payment8001/src/main/java/com/yxpj/springcloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.yxpj.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yxpj
+ * @date 2022-01-16
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
