dc624a3485f4a5bde49fdb0f077f2e3dafbf946f
ada626 <ada.yxin@gmail.com>
2022-01-20 15:20:42 +0800

    服åand hancler for degraded service8001_timeout

--- atguiguCloud/cloud-provider-hystrix-payment8001/src/main/java/com/yxpj/springcloud/service/PaymentService.java
+++ atguiguCloud/cloud-provider-hystrix-payment8001/src/main/java/com/yxpj/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.yxpj.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -10,6 +12,7 @@ public class PaymentService {
         return "线程池：" + Thread.currentThread().getName() + "paymentinfo_OK,id:" + id + "\t" + "(￣▽￣)呵呵" ;
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")})
     public String paymentInfo_TimeOut(Integer id) {
         int timeNumber = 5;
         try {
@@ -18,5 +21,8 @@ public class PaymentService {
             e.printStackTrace();
         }
         return "线程池：" + Thread.currentThread().getName() + "paymentinfo_Timeout,id:" + id + "\t" + "耗时(秒)" + timeNumber;
+    }
+    private String paymentInfo_TimeOutHandler(Integer id) {
+        return "线程池：" + Thread.currentThread().getName() + "paymentInfo_TimeOutHandler,id:" + id + "\t"+"系统繁忙请稍后再来 /(ㄒoㄒ)/";
     }
 }
